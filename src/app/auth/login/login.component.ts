ee0f1dc9a66ebd0f2f4184f75f1d3eaa61231cec
ricardo <mrbtextil@gmail.com>
2024-08-20 23:27:00 -0500

    nav and shareds
    add a component nav, add mixes and colors and use that in login.comonent.html

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
